20e35d5a4c29894d476654bfa8de091e9a4b53a0
Ixcatl <enderman99102@gmail.com>
2023-11-12 22:44:48 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -58,9 +58,10 @@ templates_path = ['_templates']
 html_theme = 'sphinx_book_theme'
 html_title = 'Us Who Remain: TTRPG Rulebook'
 html_theme_options = {
-    "use_download_button": False,
-    "use_fullscreen_button": False,
-    "show_toc_level": 2
+    'display_version': False
+    # "use_download_button": False,
+    # "use_fullscreen_button": False,
+    # "show_toc_level": 2
 }
 
 favicons = [
