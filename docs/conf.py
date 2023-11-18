add93e49a932dbe8fa11d51107a738b8c62ce9c9
Ixcatl <enderman99102@gmail.com>
2023-11-18 00:35:29 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,7 @@ hoverxref_roles = [
     # Not supported yet: https://github.com/readthedocs/sphinx-hoverxref/issues/18
     "doc",
     # Glossary terms
-    "term",
-    "hoverxref"
+    "term"
 ]
 hoverxref_role_types = {
     "mod": "modal",  # for Python Sphinx Domain
