5f7001a50a0cbdb4720a2332d6a64a7996418439
Ixcatl <enderman99102@gmail.com>
2023-11-18 00:56:26 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,24 +2,24 @@
 
 # -- Project information
 
-project = "Us Who Remain: TTRPG Rulebook"
-copyright = "2023, Ixcatl"
-author = "Ixcatl"
+project = 'Us Who Remain: TTRPG Rulebook'
+copyright = '2023, Ixcatl'
+author = 'Ixcatl'
 
-release = "1.0"
-version = "1.0"
+release = '1.0'
+version = '1.0'
 
 # -- General configuration
 
 extensions = [
-    "sphinx.ext.duration",
-    "sphinx.ext.doctest",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.intersphinx",
-    "hoverxref.extension",
-    "sphinx_favicon",
-    "sphinx_design"
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'hoverxref.extension',
+    'sphinx_favicon',
+    'sphinx_design'
 ]
 
 hoverxref_auto_ref = False
@@ -38,39 +38,41 @@ hoverxref_roles = [
 ]
 hoverxref_role_types = {
     "hoverxref": "tooltip",
+    "mod": "modal",  # for Python Sphinx Domain
     "doc": "modal",  # for whole docs
+    "class": "tooltip",  # for Python Sphinx Domain
     "ref": "tooltip",  # for hoverxref_auto_ref config
     "confval": "tooltip",  # for custom object
     "term": "tooltip"  # for glossaries
 }
 
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/3/", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/master/", None)
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None)
 }
-intersphinx_disabled_domains = ["std"]
+intersphinx_disabled_domains = ['std']
 
-templates_path = ["_templates"]
+templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = "sphinx_rtd_theme" # sphinx-rtd-theme==1.3.0rc1 sphinx==7.1.2
-# html_theme = "sphinx_book_theme" # sphinx-book-theme==1.0.1 sphinx==6.2.1
-html_title = "Us Who Remain: TTRPG Rulebook"
+html_theme = 'sphinx_rtd_theme' # sphinx-rtd-theme==1.3.0rc1 sphinx==7.1.2
+# html_theme = 'sphinx_book_theme' # sphinx-book-theme==1.0.1 sphinx==6.2.1
+html_title = 'Us Who Remain: TTRPG Rulebook'
 html_theme_options = {
-    "display_version": False,
-    "collapse_navigation": False,
-    "sticky_navigation": False,
-    "navigation_depth": -1,
-    "prev_next_buttons_location": "None"
+    'display_version': False,
+    'collapse_navigation': False,
+    'sticky_navigation': False,
+    'navigation_depth': -1,
+    'prev_next_buttons_location': 'None'
     # "use_download_button": False,
     # "use_fullscreen_button": False,
     # "show_toc_level": 2
 }
 
-html_static_path = ["_static"]
+html_static_path = ['_static']
 html_css_files = [
-    "css/custom.css"
+    'css/custom.css'
 ]
 
 favicons = [
@@ -86,4 +88,4 @@ favicons = [
 ]
 
 # -- Options for EPUB output
-epub_show_urls = "footnote"
+epub_show_urls = 'footnote'
